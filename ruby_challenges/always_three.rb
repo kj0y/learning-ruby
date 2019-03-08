55a94850c7dfc13bec7e7b99f5b3f40c7a088bee
Kandis Flick <kandisflick@gmail.com>
2019-03-07 17:54:18 -0800

    additional completed challenges

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -1,3 +1,8 @@
+def always_three(user_number)
+    puts "Always #{((((user_number+5)*2)-4)/2)-user_number}"
+end
+
 puts "Give me a number:"
 user_number = gets.to_i
-puts "Always #{((((user_number+5)*2)-4)/2)-user_number}"
+
+always_three(user_number)
---
+++ ruby_challenges/first_ruby_method.rb
@@ -0,0 +1,6 @@
+def simple_addition(number1, number2)
+    sum = number1 + number2
+    puts sum
+end
+
+simple_addition(201, 7)
